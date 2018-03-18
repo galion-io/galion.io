0ccecc6058c6caee9ce5cc3a08950b784a22bbaa
Romain <romain@galion.io>
2018-03-18 20:58:34 +0100

    Design adjustement on the landing page, up to the mid-newsletter block

--- src/js/features.js
+++ src/js/features.js
@@ -4,5 +4,10 @@
     $('#feature-img').attr('src', 'img/' + name + '.png');
     $('#features nav a').removeClass('active');
     $(el).addClass('active');
+   
+    $('#dashboard').css('display', 'none');
+	  $('#wallets').css('display', 'none');
+    $('#mobile').css('display', 'none');
+    $('#'+ name).css('display', 'block');
   };
 })(window);
